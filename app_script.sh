eb4249a3df407d99b23f54921021b19a4904224b
mortecitheodor <mortecitheodor@yahoo.com>
2023-06-20 11:47:52 +0300

    Baby steps

--- app_script.sh
+++ app_script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Utilizați Zenity pentru a afișa o fereastră de dialog cu titlu, mesaj și câmp de text de dimensiuni mai mari
-response=$(zenity --entry --title="Aplicatie Practica 2023" --text="Environment de invatare pentru studenti" --width=400 --height=200)
+response=$(zenity --entry --title="Aplicatie Practica 2023" --text="Environment de invatare pentru studenti: " --width=400 --height=200)
 
-# Verificați dacă utilizatorul a introdus un răspuns și afișați-l
+# Verificați dacă utilizatorul a introdus un răspuns
 if [[ -n "$response" ]]; then
     echo "Răspunsul utilizatorului: $response"
 else
-    echo "Nu ați introdus niciun răspuns."
+    # Utilizați Zenity pentru a afișa un nou window cu mesajul "Nu ați introdus niciun mesaj!"
+    zenity --info --title="Eroare" --text="Nu ați introdus niciun mesaj!"
 fi
